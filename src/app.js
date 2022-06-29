bcae5981f2b64983d67797e1c4d6fcfbc6e781f2
Arthuramaralx <Arthuramaralx@outlook.com>
2022-06-29 17:12:51 -0300

    commit erro docker

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import userRouter from "./routers/users.routes";
-import loginRouter from"./routers/login.routes"
-
+import loginRouter from "./routers/login.routes";
+import { connectDatabase } from "./database"
 const app = express();
 app.use(express.json());
 app.use("/users", userRouter);
-app.use("/login", loginRouter)
-app.listen(3000)
+app.use("/login", loginRouter);
+app.listen(3000, async ()  =>{
+    console.log('Executando na porta 3000')
+   await connectDatabase();
+});
 
 export default app;
